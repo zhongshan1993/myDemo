1152048f6c28ea41b50bc67da89c6bed0932bfcd
zhongshan <imzhongshan@126.com>
2015-12-21 13:31:52 +0800

    ExpanderSimple

---
+++ AngularJs/expanderSimple/ExpanderSimple.js
@@ -0,0 +1,26 @@
+var expanderModule = angular.module('ExpanderModule', []);
+
+expanderModule.directive('expander', function() {
+	return {
+		restrict: 'AE',
+		replace: true,
+		transclude: true,
+		scope: {
+			title: '=expanderTitle'
+		},
+		template: '<div>'
+				+ '<div class="title" ng-click="toggle()">{{title}}</div>'
+				+ '<div class="body" ng-show="showMe" ng-transclude></div>'
+				+ '<div>',
+		link: function(scope, element, attrs) {
+			scope.showMe = false;
+			scope.toggle = function() {
+				scope.showMe = !scope.showMe;
+			}
+		}
+	}
+});
+expanderModule.controller('SomeController', ['$scope', function($scope){
+	$scope.title = "点击展开";
+	$scope.text = "这里是内部内容。"
+}])
