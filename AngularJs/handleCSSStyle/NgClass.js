56da0cc7c66c4ebf68ce8d9d075837a1d6abfbd7
zhongshan <imzhongshan@126.com>
2015-12-23 22:45:04 +0800

    handle css style

---
+++ AngularJs/handleCSSStyle/NgClass.js
@@ -0,0 +1,16 @@
+var myCSSModule = angular.module('MyCSSModule', []);
+
+myCSSModule.controller('HeaderController', ['$scope', function($scope){
+	$scope.isError = false;
+	$scope.isWarning = false;
+	$scope.showError = function() {
+		$scope.messageText = "This is a error";
+		$scope.isError = true;
+		$scope.isWarning = false;
+	}
+	$scope.showWarning = function() {
+		$scope.messageText = "Just a Warining, please carry on!";
+		$scope.isError = false;
+		$scope.isWarning = true;
+	}
+}])
